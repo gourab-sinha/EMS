c400535d32753b9dda1d77b0eae7acfe134a4978
gourab-sinha <gourab19964u@gmail.com>
2020-12-24 00:13:48 +0530

    fullName virtual added to employee schema

--- backend/models/employee.js
+++ backend/models/employee.js
@@ -1,9 +1,17 @@
 // Employee model structure
 const mongoose = require('mongoose');
+const uniqueValidator=  require('mongoose-unique-validator');
 const employeeSchema = new mongoose.Schema({
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
-    role: {type: String, required: true}
+    role: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
 });
+employeeSchema.plugin(uniqueValidator);
+
+employeeSchema.virtual("fullName", function(){
+    return this.firstName + ' ' + this.lastName;
+});
+
 
 module.exports = mongoose.model("Employee", employeeSchema);
