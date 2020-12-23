8f1d36478145d99ef5abaf550586847e4e142a74
gourab-sinha <gourab19964u@gmail.com>
2020-12-24 00:03:27 +0530

    db connection

---
+++ backend/db/connection.js
@@ -0,0 +1,9 @@
+// Database connection
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/ems", {useNewUrlParser: true}).then(
+    ()=>{
+        console.log("Connected to database");
+    }
+).catch(()=>{
+    console.log("Connection failed!");
+});
